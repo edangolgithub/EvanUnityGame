d49f9b1d95d47cda6bb0c6699dbec41d9590235a
edangolgithub <dangolevan@gmail.com>
2019-04-09 19:34:45 +0545

    first

---
+++ Assets/Scripts/first.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first : MonoBehaviour
+{
+   public Rigidbody rb;
+    void Start()
+    {
+        rb.useGravity=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
